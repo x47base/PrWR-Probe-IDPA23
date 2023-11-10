b374b505852bb0cba0a70ea8fc0b8cac26196904
x47base <dr.greenjet@gmail.com>
2023-11-10 11:24:38 +0100

    Update Base Header & Footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,11 +2,12 @@ import './footer.css';
 
 export default function Footer({ }) {
     return (
-        <footer>
-            <div className="">
-
+        <footer className="flex flex-col justify-center items-center p-2 bg-brand">
+            <div className="flex flex-wrap gap-3 font-normal text-white justify-center items-center">
+                <a href="#">Datenschutz</a>
+                <a href="#">Impressum</a>
             </div>
-            <span>&copy; I2a 2023</span>
+            <span className="text-white font-bold">&copy; I2a 2023</span>
         </footer>
     )
 }
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,8 +3,8 @@ import './header.css';
 export default function Header({ }) {
     return (
         <header>
-            <nav className="">
-
+            <nav className="flex flex-row justify-between w-full p-2 pl-4 pr-4 bg-brand">
+                <span id="navBrand" className="font-extrabold text-2xl text-white dark:text-black">Römische Wirtschaft</span>
             </nav>
         </header>
     )
