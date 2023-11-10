50401a5581cf12436257d0d442e01a41d4336b9a
x47base <dr.greenjet@gmail.com>
2023-11-10 17:52:59 +0100

    Update Base Header & Footer

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,6 +5,12 @@ export default function Header({ }) {
         <header>
             <nav className="flex flex-row justify-between w-full p-2 pl-4 pr-4 bg-brand">
                 <span id="navBrand" className="font-extrabold text-2xl text-white dark:text-black">Römische Wirtschaft</span>
+                <div className="grid grid-cols-[auto,auto,auto] gap-4 justify-center items-center text-white font-semibold">
+                    <a href="/" target="_self">Home</a>
+                    <a href="#" target="_self">Lorem</a>
+                    <a href="#" target="_self">Ipsum</a>
+                    
+                </div>
             </nav>
         </header>
     )
