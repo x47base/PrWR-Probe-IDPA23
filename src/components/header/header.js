00d5f4a30f515ab83ae4528e5b62f53dc12c9f52
BarmettlerL-bzz <BarmettlerL@bzz.ch>
2023-11-17 10:06:06 +0100

    dropdown 0.01

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,14 +4,15 @@ export default function Header() {
     return (
         <header>
             <nav className="flex flex-row justify-between w-full p-2 pl-4 pr-4 bg-header dark:bg-dark-header">
-                <span id="navBrand" className="font-extrabold text-2xl text-white dark:text-black">Römische Wirtschaft</span>
-                <div className="grid grid-cols-[auto,auto,auto] gap-4 justify-center items-center text-white font-semibold">
-                    <a href="/" target="_self" className="text-white font-semibold">Home</a>
-                    <a href="/" target="_self" className="text-white font-semibold">Lorem</a>
-                    <a href="/" target="_self" className="text-white font-semibold">
+                <span id="navBrand" className="font-extrabold text-2xl text-textLight dark:text-textDark">Römische Wirtschaft</span>
+                <div className="grid grid-cols-[auto,auto,auto] gap-4 justify-center items-center text-textLight dark:text-textDark font-semibold">
+                    <a href="/" target="_self" className="text-textLight dark:text-textDark font-semibold">Home</a>
+                    <a href="/" target="_self" className="text-textLight dark:text-textDark font-semibold">Lorem</a>
+                    <a href="/" target="_self" className="text-textLight dark:text-textDark font-semibold">
                         <span className="text-white font-semibold">Ipsum</span>
                         <div className="dropDownContainer">
-
+                            <a href="/" target="_self" className="text-textLight dark:text-textDark font-semibold">Test 1></a>
+                            <a href="/" target="_self" className="text-textLight dark:text-textDark font-semibold">Test 2></a>
                         </div>
                     </a>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,16 +16,16 @@ module.exports = {
         "dark-brand": "#FF0000",
         // Background Color
         background: "#FFF5E6",
-        "dark-background": "#FF0000",
+        "dark-background": "#0f141a",
         // Header Color
         header: "#CAC198",
-        "dark-header": "#FF0000",
+        "dark-header": "#181f26",
         // Hover Color
         hover: "#313131",
         "dark-hover": "#FF0000",
         // Text Color
         textLight: "#FFFFFF",
-        textDark: "#FF0000",
+        textDark: "#e9eef4",
         // Faded Color
         textFaded: "#ABABAB",
         "dark-textFaded": "#FF0000",
