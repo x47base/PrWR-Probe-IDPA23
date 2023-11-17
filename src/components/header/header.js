44e69c9d6b7503552094c11452c2516cc8a896e0
BarmettlerL-bzz <BarmettlerL@bzz.ch>
2023-11-17 10:58:27 +0100

    opacity in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,9 +6,9 @@ export default function Header() {
             <nav className="flex flex-row justify-between w-full p-2 pl-4 pr-4 bg-header dark:bg-dark-header">
                 <span id="navBrand" className="font-extrabold text-2xl text-textLight dark:text-textDark">Römische Wirtschaft</span>
                 <div className="grid grid-cols-[auto,auto,auto] gap-4 justify-center items-center text-textLight dark:text-textDark font-semibold">
-                    <a href="/" target="_self" className="text-textLight dark:text-textDark font-semibold">Home</a>
-                    <a href="/" target="_self" className="text-textLight dark:text-textDark font-semibold">Lorem</a>
-                    <a href="/" target="_self" className="text-textLight dark:text-textDark font-semibold">Ipsum</a>
+                    <a href="/" target="_self" className="opacity-70 hover:opacity-100 text-textLight dark:text-textDark font-semibold">Home</a>
+                    <a href="/" target="_self" className="opacity-70 hover:opacity-100 text-textLight dark:text-textDark font-semibold">Lorem</a>
+                    <a href="/" target="_self" className="opacity-70 hover:opacity-100 text-textLight dark:text-textDark font-semibold">Ipsum</a>
                 </div>
             </nav>
         </header>
