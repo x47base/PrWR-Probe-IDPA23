d18f76187fc06aa9a0a89f4067a488197c06a2b0
x47base <dr.greenjet@gmail.com>
2023-11-17 12:05:50 +0100

    Added some Text Content for Website, handled with a arrayand automaticly inserted to the page by using array.map()

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header>
             <nav className="flex flex-row justify-between w-full p-2 pl-4 pr-4 bg-header dark:bg-dark-header">
-                <span id="navBrand" className="font-extrabold text-2xl text-textHeader dark:text-textDark">Römische Wirtschaft</span>
+                <a href="/" target="_self" id="navBrand" className="font-extrabold text-2xl text-textHeader dark:text-textDark">Römische Wirtschaft</a>
                 <div className="grid grid-cols-[auto,auto,auto] gap-4 justify-center items-center text-textHeader dark:text-textDark font-semibold">
                     <a href="/" target="_self" className="opacity-70 hover:opacity-100 text-textHeader dark:text-textDark font-semibold">Home</a>
                     <a href="/" target="_self" className="opacity-70 hover:opacity-100 text-textHeader dark:text-textDark font-semibold">Lorem</a>
