59ea0ef64b74fafa6bafc43e7d1a79f4742b3282
x47base <dr.greenjet@gmail.com>
2023-12-01 09:45:57 +0100

    Header Update

--- app/datenschutz/page.tsx
+++ app/datenschutz/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <header className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
                 <nav className="fixed left-0 top-0 flex w-full justify-between border-b border-gray-300 bg-gradient-to-b from-zinc-200 p-2 pl-4 pr-4 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    <span className="text-2xl font-extrabold">Römische Wirtschaft</span>
+                    <a href="/" target="_self" className="text-2xl font-extrabold">Römische Wirtschaft</a>
                     <div className="grid grid-cols-[auto,auto,auto] gap-4 justify-center items-center">
                         <a href="/" target="_self" className="opacity-70 hover:opacity-100 text-base">Home</a>
                         <a href="/innenhandel" target="_self" className="opacity-70 hover:opacity-100 text-base">Innenhandel</a>
